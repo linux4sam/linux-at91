763d331887a7018f313279364e6e6d432952ee02
Yannick Lanz <yannick.lanz@wifx.net>
2020-06-11 10:15:08 +0200

    net: macb: Incomplete received frame is not a critical error Warning log level is more appropriate to avoid any confusion.
    Signed-off-by: Yannick Lanz <yannick.lanz@wifx.net>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -1144,7 +1144,7 @@ static int gem_rx(struct macb_queue *queue, struct napi_struct *napi,
 		count++;
 
 		if (!(ctrl & MACB_BIT(RX_SOF) && ctrl & MACB_BIT(RX_EOF))) {
-			netdev_err(bp->dev,
+			netdev_warn(bp->dev,
 				   "not whole frame pointed by descriptor\n");
 			bp->dev->stats.rx_dropped++;
 			queue->stats.rx_dropped++;
