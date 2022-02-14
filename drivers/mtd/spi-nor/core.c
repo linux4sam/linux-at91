75470265e62aa9e7d6af93d6b06843172ee673ac
Tudor Ambarus <tudor.ambarus@microchip.com>
2022-02-14 18:50:29 +0200

    mtd: spi-nor: core: Introduce SPI_NOR_DTR_BSWAP16 no_sfdp_flag
    Introduce SPI_NOR_DTR_BSWAP16 flag for flashes that don't define the
    mandatory BFPT table. When set it indicates that the byte order of 16-bit
    words is swapped when read in 8D-8D-8D mode compared to 1-1-1 mode.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    [durai.manickamkr@microchip.com: Fixed conflicts and ported to 6.12.4]
    Signed-off-by: Durai Manickam KR <durai.manickamkr@microchip.com>
    [nayabbasha.sayed@microchip.com: fix conflicts while porting to 6.18.1]
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -2808,7 +2808,7 @@ static void spi_nor_no_sfdp_init_params(struct spi_nor *nor)
 	struct spi_nor_flash_parameter *params = nor->params;
 	struct spi_nor_erase_map *map = &params->erase_map;
 	const struct flash_info *info = nor->info;
-	const u8 no_sfdp_flags = info->no_sfdp_flags;
+	const u16 no_sfdp_flags = nor->info->no_sfdp_flags;
 	u8 i, erase_mask;
 
 	if (no_sfdp_flags & SPI_NOR_DUAL_READ) {
@@ -2848,6 +2848,9 @@ static void spi_nor_no_sfdp_init_params(struct spi_nor *nor)
 		spi_nor_set_pp_settings(&params->page_programs[SNOR_CMD_PP_8_8_8_DTR],
 					SPINOR_OP_PP, SNOR_PROTO_8_8_8_DTR);
 	}
+
+	if (no_sfdp_flags & SPI_NOR_DTR_BSWAP16)
+		nor->flags |= SNOR_F_SWAP16;
 
 	/*
 	 * Sector Erase settings. Sort Erase Types in ascending order, with the
