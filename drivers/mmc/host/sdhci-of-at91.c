763a3c05c8f9d0feb279c6f308fa5d48d14e07d3
Romain Sioen <romain.sioen@microchip.com>
2025-01-10 14:58:42 +0100

    mmc: sdhci-of-at91: update clock management on pm runtime
    Sama7 boards are impacted by an unbalanced prepare/unprepare
    and enable/disable clock management in sdhci driver due to
    the lack of support for SDMMC clock runtime disabling,
    managed instead by the PMC driver.
    
    Add conditions in the preset function to manage clock
    depending on if the board support runtime clock disabling
    and if we come from the probing or restoring procedure.
    
    Add boolean start_clks variable to manage condition
    in preset function.
    
    Signed-off-by: Romain Sioen <romain.sioen@microchip.com>
    Acked-by: Aubin Constans <aubin.constans@microchip.com>

--- drivers/mmc/host/sdhci-of-at91.c
+++ drivers/mmc/host/sdhci-of-at91.c
@@ -243,7 +243,7 @@ static const struct of_device_id sdhci_at91_dt_match[] = {
 };
 MODULE_DEVICE_TABLE(of, sdhci_at91_dt_match);
 
-static int sdhci_at91_set_clks_presets(struct device *dev)
+static int sdhci_at91_set_clks_presets(struct device *dev, bool start_clks)
 {
 	struct sdhci_host *host = dev_get_drvdata(dev);
 	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
@@ -253,7 +253,14 @@ static int sdhci_at91_set_clks_presets(struct device *dev)
 	unsigned int			gck_rate, clk_base_rate;
 	unsigned int			preset_div;
 
-	clk_prepare_enable(priv->hclock);
+	/*
+	 * Manage clock prepare/enable procedure depending on if the board
+	 * support runtime clock disabling and if we come from the probing
+	 * or restoring procedure (for backup+self refresh).
+	 */
+	if (start_clks)
+		clk_prepare_enable(priv->hclock);
+
 	caps0 = readl(host->ioaddr + SDHCI_CAPABILITIES);
 	caps1 = readl(host->ioaddr + SDHCI_CAPABILITIES_1);
 
@@ -323,8 +330,11 @@ static int sdhci_at91_set_clks_presets(struct device *dev)
 		       host->ioaddr + SDHCI_PRESET_FOR_HS400);
 	}
 
-	clk_prepare_enable(priv->mainck);
-	clk_prepare_enable(priv->gck);
+	/* Same clock management as for hclock */
+	if (start_clks) {
+		clk_prepare_enable(priv->mainck);
+		clk_prepare_enable(priv->gck);
+	}
 
 	return 0;
 }
@@ -371,7 +381,7 @@ static int sdhci_at91_runtime_resume(struct device *dev)
 	int ret;
 
 	if (priv->restore_needed) {
-		ret = sdhci_at91_set_clks_presets(dev);
+		ret = sdhci_at91_set_clks_presets(dev, priv->soc_data->pm_runtime_disable_clks);
 		if (ret)
 			return ret;
 
@@ -468,7 +478,7 @@ static int sdhci_at91_probe(struct platform_device *pdev)
 		return dev_err_probe(&pdev->dev, PTR_ERR(priv->gck),
 				     "failed to get multclk\n");
 
-	ret = sdhci_at91_set_clks_presets(&pdev->dev);
+	ret = sdhci_at91_set_clks_presets(&pdev->dev, true);
 	if (ret)
 		return ret;
 
