a231683bc0f93c94d4615b11a7ff4c10a5602275
Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>
2025-04-01 11:46:48 +0530

    input: misc: atmel_ptc: fix compilation error with GPIOF_DIR_*
    Replace GPIOF_DIR_* with GPIOF_IN_* since their support is removed from
    gpiolib.
    
    Fixes: 5aa12e163f10 ("input: misc: introduce Atmel PTC driver")
    Signed-off-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>

--- drivers/input/misc/atmel_ptc.c
+++ drivers/input/misc/atmel_ptc.c
@@ -521,7 +521,8 @@ static int atmel_ptc_request_pins(struct atmel_ptc *ptc)
 			if (ptc->x_lines_requested[j])
 				continue;
 
-			if (gpio_request_one(ptc->pins->x_lines[j].id, GPIOF_DIR_IN, ptc->pins->x_lines[j].name)) {
+			if (gpio_request_one(ptc->pins->x_lines[j].id, GPIOF_IN,
+						ptc->pins->x_lines[j].name)) {
 				dev_err(ptc->dev, "Can't get %s\n", ptc->pins->x_lines[j].name);
 				return -ENXIO;
 			}
@@ -532,7 +533,8 @@ static int atmel_ptc_request_pins(struct atmel_ptc *ptc)
 			if (ptc->y_lines_requested[j])
 				continue;
 
-			if (gpio_request_one(ptc->pins->y_lines[j].id, GPIOF_DIR_IN, ptc->pins->y_lines[j].name)) {
+			if (gpio_request_one(ptc->pins->y_lines[j].id, GPIOF_IN,
+						ptc->pins->y_lines[j].name)) {
 				dev_err(ptc->dev, "Can't get %s\n", ptc->pins->y_lines[j].name);
 				return -ENXIO;
 			}
