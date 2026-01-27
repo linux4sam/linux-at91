atmel-maxtouch/maXTouch_linux_6.12

ARM: at91: add support for Software License Levels
Add support for Software License Level for existing and new SoCs.

Signed-off-by: Dharma Balasubiramani <dharma.b@microchip.com>
Reviewed-by: Balamanikandan Gunasundar <balamanikandan.gunasundar@microchip.com>

drivers/soc/atmel/soc.h
@@ -66,26 +66,103 @@ at91_soc_init(const struct at91_soc *socs);
 #define AT91SAM9CN11_EXID_MATCH		0x00000009
 
 #define SAM9X60_EXID_MATCH		0x00000000
+#define SAM9X60_SL1_EXID_MATCH		0x000000C4
+#define SAM9X60_SL2_EXID_MATCH		0x000000C5
+#define SAM9X60_SL3_EXID_MATCH		0x000000C6
+
 #define SAM9X60_D5M_EXID_MATCH		0x00000001
+#define SAM9X60_D5M_SL1_EXID_MATCH	0x000000C8
+#define SAM9X60_D5M_SL2_EXID_MATCH	0x000000C9
+#define SAM9X60_D5M_SL3_EXID_MATCH	0x000000CA
+
 #define SAM9X60_D1G_EXID_MATCH		0x00000010
+#define SAM9X60_D1G_SL1_EXID_MATCH	0x000000CC
+#define SAM9X60_D1G_SL2_EXID_MATCH	0x000000CD
+#define SAM9X60_D1G_SL3_EXID_MATCH	0x000000CE
+
 #define SAM9X60_D6K_EXID_MATCH		0x00000011
+#define SAM9X60_D6K_SL1_EXID_MATCH	0x000000C0
+#define SAM9X60_D6K_SL2_EXID_MATCH	0x000000C1
+#define SAM9X60_D6K_SL3_EXID_MATCH	0x000000C2
 
 #define SAM9X70_EXID_MATCH		0x00000080
+#define SAM9X70_SL1_EXID_MATCH		0x000000C4
+#define SAM9X70_SL2_EXID_MATCH		0x000000C5
+#define SAM9X70_SL3_EXID_MATCH		0x000000C6
+
 #define SAM9X72_EXID_MATCH		0x00000082
-#define SAM9X75_D1G_EXID_MATCH		0x00000018
-#define SAM9X75_D2G_EXID_MATCH		0x00000020
-#define SAM9X75_D5M_EXID_MATCH		0x00000010
+#define SAM9X72_SL1_EXID_MATCH		0x000000D8
+#define SAM9X72_SL2_EXID_MATCH		0x000000D9
+#define SAM9X72_SL3_EXID_MATCH		0x000000DA
+
 #define SAM9X75_EXID_MATCH		0x00000085
+#define SAM9X75_SL1_EXID_MATCH		0x000000DC
+#define SAM9X75_SL2_EXID_MATCH		0x000000DD
+#define SAM9X75_SL3_EXID_MATCH		0x000000DE
+
+#define SAM9X75_D1G_EXID_MATCH		0x00000018
+#define SAM9X75_D1G_SL1_EXID_MATCH	0x000000CC
+#define SAM9X75_D1G_SL2_EXID_MATCH	0x000000CD
+#define SAM9X75_D1G_SL3_EXID_MATCH	0x000000CE
+
+#define SAM9X75_D2G_EXID_MATCH		0x00000020
+#define SAM9X75_D2G_SL1_EXID_MATCH	0x000000D0
+#define SAM9X75_D2G_SL2_EXID_MATCH	0x000000D1
+#define SAM9X75_D2G_SL3_EXID_MATCH	0x000000D2
+
+#define SAM9X75_D5M_EXID_MATCH		0x00000010
+#define SAM9X75_D5M_SL1_EXID_MATCH	0x000000C8
+#define SAM9X75_D5M_SL2_EXID_MATCH	0x000000C9
+#define SAM9X75_D5M_SL3_EXID_MATCH	0x000000CA
 
 #define SAMA7D65_EXID_MATCH		0x00000080
+#define SAMA7D65_SL1_EXID_MATCH		0x000000C4
+#define SAMA7D65_SL2_EXID_MATCH		0x000000C5
+#define SAMA7D65_SL3_EXID_MATCH		0x000000C6
 
-#define SAMA7G51_EXID_MATCH		0x3
-#define SAMA7G52_EXID_MATCH		0x2
-#define SAMA7G53_EXID_MATCH		0x1
-#define SAMA7G54_EXID_MATCH		0x0
+#define SAMA7D65D1G_EXID_MATCH		0x00000018
+#define SAMA7D65D1G_SL1_EXID_MATCH	0x000000CC
+#define SAMA7D65D1G_SL2_EXID_MATCH	0x000000CD
+#define SAMA7D65D1G_SL3_EXID_MATCH	0x000000CE
+
+#define SAMA7D65D2G_EXID_MATCH		0x00000020
+#define SAMA7D65D2G_SL1_EXID_MATCH	0x000000D0
+#define SAMA7D65D2G_SL2_EXID_MATCH	0x000000D1
+#define SAMA7D65D2G_SL3_EXID_MATCH	0x000000D2
+
+#define SAMA7D65D4G_EXID_MATCH		0x00000028
+#define SAMA7D65D4G_SL1_EXID_MATCH	0x000000D4
+#define SAMA7D65D4G_SL2_EXID_MATCH	0x000000D5
+#define SAMA7D65D4G_SL3_EXID_MATCH	0x000000D6
+
+#define SAMA7D65D5M_EXID_MATCH		0x00000010
+#define SAMA7D65D5M_SL1_EXID_MATCH	0x000000C8
+#define SAMA7D65D5M_SL2_EXID_MATCH	0x000000C9
+#define SAMA7D65D5M_SL3_EXID_MATCH	0x000000CA
+
+#define SAMA7G51_EXID_MATCH		0x00000003
+#define SAMA7G52_EXID_MATCH		0x00000002
+#define SAMA7G53_EXID_MATCH		0x00000001
+
+#define SAMA7G54_EXID_MATCH		0x00000000
+#define SAMA7G54_SL1_EXID_MATCH		0x000000C4
+#define SAMA7G54_SL2_EXID_MATCH		0x000000C5
+#define SAMA7G54_SL3_EXID_MATCH		0x000000C6
+
 #define SAMA7G54_D1G_EXID_MATCH		0x00000018
+#define SAMA7G54_D1G_SL1_EXID_MATCH	0x000000CC
+#define SAMA7G54_D1G_SL2_EXID_MATCH	0x000000CD
+#define SAMA7G54_D1G_SL3_EXID_MATCH	0x000000CE
+
 #define SAMA7G54_D2G_EXID_MATCH		0x00000020
+#define SAMA7G54_D2G_SL1_EXID_MATCH	0x000000D0
+#define SAMA7G54_D2G_SL2_EXID_MATCH	0x000000D1
+#define SAMA7G54_D2G_SL3_EXID_MATCH	0x000000D2
+
 #define SAMA7G54_D4G_EXID_MATCH		0x00000028
+#define SAMA7G54_D4G_SL1_EXID_MATCH	0x000000D4
+#define SAMA7G54_D4G_SL2_EXID_MATCH	0x000000D5
+#define SAMA7G54_D4G_SL3_EXID_MATCH	0x000000D6
 
 #define AT91SAM9XE128_CIDR_MATCH	0x329973a0
 #define AT91SAM9XE256_CIDR_MATCH	0x329a93a0

linux4sam/linux-at91

drivers/soc/atmel/soc.h
@@ -66,26 +66,103 @@ at91_soc_init(const struct at91_soc *socs);
 #define AT91SAM9CN11_EXID_MATCH		0x00000009
 
 #define SAM9X60_EXID_MATCH		0x00000000
+#define SAM9X60_SL1_EXID_MATCH		0x000000C4
+#define SAM9X60_SL2_EXID_MATCH		0x000000C5
+#define SAM9X60_SL3_EXID_MATCH		0x000000C6
+
 #define SAM9X60_D5M_EXID_MATCH		0x00000001
+#define SAM9X60_D5M_SL1_EXID_MATCH	0x000000C8
+#define SAM9X60_D5M_SL2_EXID_MATCH	0x000000C9
+#define SAM9X60_D5M_SL3_EXID_MATCH	0x000000CA
+
 #define SAM9X60_D1G_EXID_MATCH		0x00000010
+#define SAM9X60_D1G_SL1_EXID_MATCH	0x000000CC
+#define SAM9X60_D1G_SL2_EXID_MATCH	0x000000CD
+#define SAM9X60_D1G_SL3_EXID_MATCH	0x000000CE
+
 #define SAM9X60_D6K_EXID_MATCH		0x00000011
+#define SAM9X60_D6K_SL1_EXID_MATCH	0x000000C0
+#define SAM9X60_D6K_SL2_EXID_MATCH	0x000000C1
+#define SAM9X60_D6K_SL3_EXID_MATCH	0x000000C2
 
 #define SAM9X70_EXID_MATCH		0x00000080
+#define SAM9X70_SL1_EXID_MATCH		0x000000C4
+#define SAM9X70_SL2_EXID_MATCH		0x000000C5
+#define SAM9X70_SL3_EXID_MATCH		0x000000C6
+
 #define SAM9X72_EXID_MATCH		0x00000082
-#define SAM9X75_D1G_EXID_MATCH		0x00000018
-#define SAM9X75_D2G_EXID_MATCH		0x00000020
-#define SAM9X75_D5M_EXID_MATCH		0x00000010
+#define SAM9X72_SL1_EXID_MATCH		0x000000D8
+#define SAM9X72_SL2_EXID_MATCH		0x000000D9
+#define SAM9X72_SL3_EXID_MATCH		0x000000DA
+
 #define SAM9X75_EXID_MATCH		0x00000085
+#define SAM9X75_SL1_EXID_MATCH		0x000000DC
+#define SAM9X75_SL2_EXID_MATCH		0x000000DD
+#define SAM9X75_SL3_EXID_MATCH		0x000000DE
+
+#define SAM9X75_D1G_EXID_MATCH		0x00000018
+#define SAM9X75_D1G_SL1_EXID_MATCH	0x000000CC
+#define SAM9X75_D1G_SL2_EXID_MATCH	0x000000CD
+#define SAM9X75_D1G_SL3_EXID_MATCH	0x000000CE
+
+#define SAM9X75_D2G_EXID_MATCH		0x00000020
+#define SAM9X75_D2G_SL1_EXID_MATCH	0x000000D0
+#define SAM9X75_D2G_SL2_EXID_MATCH	0x000000D1
+#define SAM9X75_D2G_SL3_EXID_MATCH	0x000000D2
+
+#define SAM9X75_D5M_EXID_MATCH		0x00000010
+#define SAM9X75_D5M_SL1_EXID_MATCH	0x000000C8
+#define SAM9X75_D5M_SL2_EXID_MATCH	0x000000C9
+#define SAM9X75_D5M_SL3_EXID_MATCH	0x000000CA
 
 #define SAMA7D65_EXID_MATCH		0x00000080
+#define SAMA7D65_SL1_EXID_MATCH		0x000000C4
+#define SAMA7D65_SL2_EXID_MATCH		0x000000C5
+#define SAMA7D65_SL3_EXID_MATCH		0x000000C6
 
-#define SAMA7G51_EXID_MATCH		0x3
-#define SAMA7G52_EXID_MATCH		0x2
-#define SAMA7G53_EXID_MATCH		0x1
-#define SAMA7G54_EXID_MATCH		0x0
+#define SAMA7D65_D1G_EXID_MATCH		0x00000018
+#define SAMA7D65_D1G_SL1_EXID_MATCH	0x000000CC
+#define SAMA7D65_D1G_SL2_EXID_MATCH	0x000000CD
+#define SAMA7D65_D1G_SL3_EXID_MATCH	0x000000CE
+
+#define SAMA7D65_D2G_EXID_MATCH		0x00000020
+#define SAMA7D65_D2G_SL1_EXID_MATCH	0x000000D0
+#define SAMA7D65_D2G_SL2_EXID_MATCH	0x000000D1
+#define SAMA7D65_D2G_SL3_EXID_MATCH	0x000000D2
+
+#define SAMA7D65_D4G_EXID_MATCH		0x00000028
+#define SAMA7D65_D4G_SL1_EXID_MATCH	0x000000D4
+#define SAMA7D65_D4G_SL2_EXID_MATCH	0x000000D5
+#define SAMA7D65_D4G_SL3_EXID_MATCH	0x000000D6
+
+#define SAMA7D65_D5M_EXID_MATCH		0x00000010
+#define SAMA7D65_D5M_SL1_EXID_MATCH	0x000000C8
+#define SAMA7D65_D5M_SL2_EXID_MATCH	0x000000C9
+#define SAMA7D65_D5M_SL3_EXID_MATCH	0x000000CA
+
+#define SAMA7G51_EXID_MATCH		0x00000003
+#define SAMA7G52_EXID_MATCH		0x00000002
+#define SAMA7G53_EXID_MATCH		0x00000001
+
+#define SAMA7G54_EXID_MATCH		0x00000000
+#define SAMA7G54_SL1_EXID_MATCH		0x000000C4
+#define SAMA7G54_SL2_EXID_MATCH		0x000000C5
+#define SAMA7G54_SL3_EXID_MATCH		0x000000C6
+
 #define SAMA7G54_D1G_EXID_MATCH		0x00000018
+#define SAMA7G54_D1G_SL1_EXID_MATCH	0x000000CC
+#define SAMA7G54_D1G_SL2_EXID_MATCH	0x000000CD
+#define SAMA7G54_D1G_SL3_EXID_MATCH	0x000000CE
+
 #define SAMA7G54_D2G_EXID_MATCH		0x00000020
+#define SAMA7G54_D2G_SL1_EXID_MATCH	0x000000D0
+#define SAMA7G54_D2G_SL2_EXID_MATCH	0x000000D1
+#define SAMA7G54_D2G_SL3_EXID_MATCH	0x000000D2
+
 #define SAMA7G54_D4G_EXID_MATCH		0x00000028
+#define SAMA7G54_D4G_SL1_EXID_MATCH	0x000000D4
+#define SAMA7G54_D4G_SL2_EXID_MATCH	0x000000D5
+#define SAMA7G54_D4G_SL3_EXID_MATCH	0x000000D6
 
 #define AT91SAM9XE128_CIDR_MATCH	0x329973a0
 #define AT91SAM9XE256_CIDR_MATCH	0x329a93a0